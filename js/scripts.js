1cd1901b707a4fdc8f98873681742048e2009b44
Sp-learner <shweta.patel392@gmail.com>
2024-11-22 20:30:12 +0100

    Update scripts.js
    Created an array with consistent keys for each object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-
-alert('Hello world');
-
-let favouriteFood = 'Apple';
-document.write(favouriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 6,
+        types: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 5,
+        types: ['water']
+    }
+];
