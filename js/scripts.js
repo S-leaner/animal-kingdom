d72b1f928d1b5dfa92d20a8115e51c798ce30eaa
Sp-learner <shweta.patel392@gmail.com>
2024-11-26 21:19:02 +0100

    Update scripts.js
    added script to print list of pokemon and add message to the biggest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ let pokemonList = [
     },
     {
         name: 'Charmander',
-        height: 6,
+        height: 4,
         types: ['fire']
     },
     {
@@ -15,3 +15,14 @@ let pokemonList = [
         types: ['water']
     }
 ];
+
+for ( let i = 0; i < pokemonList.length; i++ ){
+    document.write( pokemonList[i].name + "  height: " + pokemonList[i].height + "<p>");
+} //to print all pokemon's name and height
+
+for( let i = 0; i< pokemonList.length; i++ ){
+    if( pokemonList[i].height >= 6 ) {
+        document.write(pokemonList[i].name + " : wow it is big");
+    }
+}//to print message for the largest pokemon
+
