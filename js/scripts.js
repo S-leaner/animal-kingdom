6f2a93b2cdc22803ad4621253aed29c6b710cb15
Sp-learner <shweta.patel392@gmail.com>
2024-12-24 17:44:10 +0100

    added IIFE to the pokemon Array
    Make changes in script and added new variable PokemonRespository to hold Array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,41 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 4,
-        types: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 5,
-        types: ['water']
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 4,
+            types: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 5,
+            types: ['water']
+        }
+  ];
+    return {
+      add: function() {
+          pokemonList.push(pokemon);
+      },
+      getAll: function() {
+          return pokemonList;
+      }
+  };
+ }) ();
 
 
-pokemonList.forEach(function(pokemon){
+ pokemonRepository.getAll().forEach(function(pokemon){
     document.write(pokemon.name + " height: " + pokemon.height + "<p>")
 });
 //to print all pokemon's name and height
 
-pokemonList.forEach(pokemon => {
-    if (pokemon.height >= 6) {
-        document.write(pokemon.name + " : wow it is big");
-    }
-});
 
-//to print message for the largest pokemon
+// to print message for the largest pokemon
 
+
+
+  
+  
